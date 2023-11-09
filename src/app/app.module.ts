d58f426c4812ef3afea7ec6e3c1dd5ff1716da60
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-11-09 08:58:40 -0300

    autenticação com a documentação do firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+
+import { initializeApp } from 'firebase/app';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(
+      {
+        apiKey: "AIzaSyBirNVB38jTM4BvzoXvs2JRmoqPehnNZQo",
+        authDomain: "autentica-c57f6.firebaseapp.com",
+        projectId: "autentica-c57f6",
+        storageBucket: "autentica-c57f6.appspot.com",
+        messagingSenderId: "533716695287",
+        appId: "1:533716695287:web:4dc5e6dd6f9abbd8a73bf2",
+        measurementId: "G-T2GBYFMZ5Q"
+      }
+    ),],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+const app = initializeApp();
