f6c9adcc029f7ea06761389cf11fc5a667c0244b
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-11-23 15:07:59 -0300

    ae

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { app } from '../app.module';
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +15,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  clinicas = collection(getFirestore(app), 'users');
+  users: any[] = [];
+
   constructor() {}
+
+  ngOnInit() {
+    this.listaClinicas();
+  }
+
+  async listaClinicas() {
+    const auth = getAuth(app);
+
+    var divNome = document.getElementById('idnome');
+
+    onSnapshot(this.clinicas, (snapshot) => {
+      this.users = [];
+
+      snapshot.forEach((doc) => {
+        const userData = doc.data() as { nome?: string };;
+
+        let displayNome = '';
+        if (userData.nome) {
+          displayNome += `<p>${userData.nome}</p>`;
+        }
+
+        if (divNome) {
+          divNome.innerHTML = displayNome;
+        } else {
+          console.error('Informação "sobre" não encontrada');
+        }
+      });
+    });
+  }
 
 }
