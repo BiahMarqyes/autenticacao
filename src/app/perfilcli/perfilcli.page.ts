7bb2f3a05a054fcd40055b486b3c28ba64bb30d8
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-11-23 00:54:34 -0300

    humm

--- src/app/perfilcli/perfilcli.page.ts
+++ src/app/perfilcli/perfilcli.page.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { app } from '../app.module';
-import { getFirestore, collection, setDoc, doc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  setDoc,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const auth = getAuth(app);
 
@@ -13,14 +19,71 @@ const firestore = getFirestore(app);
   styleUrls: ['./perfilcli.page.scss'],
 })
 export class PerfilcliPage implements OnInit {
-
   public alertButtons = ['OK'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async atualizar() {
+    const user = auth.currentUser;
+
+    if (user) {
+      const uid = user.uid;
+
+      const users = collection(firestore, 'users');
+
+      const userDoc = doc(users, uid);
+
+      const docSnapshot = await getDoc(userDoc);
+
+      var divSobre = document.getElementById('idsobre');
+      var divHorario = document.getElementById('idhorario');
+      var divEndereco = document.getElementById('idendereco');
+
+      if (docSnapshot.exists()) {
+        const docData = docSnapshot.data() as { sobre?: string, horario?: string, rua?: string, numero?: string, bairro?: string };
+
+        let displaySobre = '';
+        if (docData.sobre) {
+          displaySobre += `<p>${docData.sobre}</p>`;
+        }
+        let displayHorario = '';
+        if (docData.horario) {
+          displayHorario += `<p>${docData.horario}</p>`;
+        }
+        let displayEndereco = '';
+        if (docData.rua) {
+          displayEndereco += `<p>${docData.rua}</p>`;
+        }
+        if (docData.numero) {
+          displayEndereco += `<p>${docData.numero}</p>`;
+        }
+        if (docData.bairro) {
+          displayEndereco += `<p>${docData.bairro}</p>`;
+        }
+
+        if (divSobre) {
+          divSobre.innerHTML = displaySobre;
+        } else {
+          console.error('Informação "sobre" não encontrada');
+        }
+        if (divHorario) {
+          divHorario.innerHTML = displayHorario;
+        } else {
+          console.error('Informação "horario" não encontrada');
+        }
+        if (divEndereco) {
+          divEndereco.innerHTML = displayEndereco;
+        } else {
+          console.error('Informação "endereco" não encontrada');
+        }
+      } else {
+        console.log('Documento não existe');
+      }
+    } else {
+      console.log('Usuário não autenticado.');
+    }
   }
-
-
 
 }
